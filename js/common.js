ec09ef7bd1a5a992cbf3b4ad6294282b5d9e9b8a
Yuriy <prezlyata.yuriy@gmail.com>
2018-02-14 15:50:50 +0200

    add main functionality

--- js/common.js
+++ js/common.js
@@ -1,12 +1,78 @@
-/***   Preloader code   ***/
+/*** Init variables ***/
 
-$(document).ready(function($) {
-    var Body = $('body');
-    Body.addClass('preloader-site');
-});
-$(window).ready(function() {
-    setTimeout(function() {
-        $('.preloader-wrapper').fadeOut();
-        $('body').removeClass('preloader-site');
-    }, 500);
-});
+var buttons = document.getElementById('buttons');
+var input_field = document.getElementById('input_field');
+var equal = document.getElementById('btn-equal');
+var clearBtn = document.getElementById('btn-clear');
+
+
+
+/*** Onload window active field ***/
+
+window.onload = function () {
+    this.input_field.focus();
+};
+
+
+
+/*** function clear input ***/
+
+function Clear() {
+    this.input_field.value = '';
+}
+
+clearBtn.onclick = function () {
+    Clear();
+};
+
+
+
+/*** function set value from button to input ***/
+
+function setValue(num){
+    if(num === 'C' || num === '=') {
+        num = '';
+    }
+    this.input_field.value += num;
+}
+
+buttons.onclick = function(e) {
+    if(e.target.tagName==='BUTTON' && e.target.classList.contains('btn')) {
+        var currentButton = e.target.innerHTML;
+        setValue(currentButton);
+        console.log(currentButton);
+    }
+};
+
+
+
+/*** function that execute values from field ***/
+
+function Equal() {
+    if(eval(this.input_field.value) === 'undefined') {
+        this.input_field.value = '';
+    } else {
+        this.input_field.value = eval(this.input_field.value);
+    }
+}
+
+equal.onclick = function() {
+    Equal();
+};
+
+
+
+/*** Work with keyboard ***/
+
+document.onkeydown = function (e) {
+    switch (e.key) {
+        case '=':
+            Equal();
+            break;
+        case 'Enter':
+            Equal();
+        default:
+            return;
+    }
+    e.preventDefault();
+};
---
+++ js/preloader.js
@@ -0,0 +1,12 @@
+/***   Preloader code   ***/
+
+$(document).ready(function($) {
+    var Body = $('body');
+    Body.addClass('preloader-site');
+});
+$(window).ready(function() {
+    setTimeout(function() {
+        $('.preloader-wrapper').fadeOut();
+        $('body').removeClass('preloader-site');
+    }, 500);
+});
